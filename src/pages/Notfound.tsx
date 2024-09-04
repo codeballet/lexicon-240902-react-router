cdf2a0e2950230c9c80920db552d11cc671ffda9
stjernholm <stjernholm@msn.com>
2024-09-04 20:05:48 +0200

    feat: Notfound component added

---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export function Notfound(): ReactElement {
+    return (
+        <h2 className="notfound">
+            This is not the page you are looking for: 404
+        </h2>
+    );
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ export * from "./About";
 export * from "./Contact";
 export * from "./Dashboard";
 export * from "./Home";
+export * from "./Notfound";
 export * from "./Settings";
 export * from "./Stats";
