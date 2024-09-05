551905551ef62748697da70009a41e0c4b5138b2
stjernholm <stjernholm@msn.com>
2024-09-05 19:46:04 +0200

    feat: Add NavLink for active menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 interface INavProps {
     loggedIn: Boolean;
@@ -9,18 +9,18 @@ interface INavProps {
 export function Nav({ loggedIn, setLoggedIn }: INavProps): ReactElement {
     return (
         <nav className="nav">
-            <Link className="nav__link" to="/">
+            <NavLink className="nav__link" to="/">
                 <button className="nav__button">Home</button>
-            </Link>
-            <Link className="nav__link" to="/about">
+            </NavLink>
+            <NavLink className="nav__link" to="/about">
                 <button className="nav__button">About</button>
-            </Link>
-            <Link className="nav__link" to="/contact">
+            </NavLink>
+            <NavLink className="nav__link" to="/contact">
                 <button className="nav__button">Contact</button>
-            </Link>
-            <Link className="nav__link" to="/dashboard">
+            </NavLink>
+            <NavLink className="nav__link" to="/dashboard">
                 <button className="nav__button">Dashboard</button>
-            </Link>
+            </NavLink>
             <button
                 className="nav__button"
                 onClick={() => setLoggedIn(!loggedIn)}
