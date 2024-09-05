b957386a8d999b287f7b70cbfbda73541e4223bf
stjernholm <stjernholm@msn.com>
2024-09-05 12:23:37 +0200

    feat: Add sets as protected route

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,8 +1,10 @@
 import { ReactElement } from "react";
+import { Protected } from "../components";
 
 export function Stats(): ReactElement {
     return (
         <>
+            <Protected />
             <h3>These are your Stats</h3>
         </>
     );
