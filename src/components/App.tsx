604ca87ad827989279efd6dd22eb61ab1a1da589
stjernholm <stjernholm@msn.com>
2024-09-05 08:36:15 +0200

    feat: Add state for loggedIn

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Nav } from ".";
 
 export function App(): ReactElement {
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
     return (
         <>
             <Nav />
