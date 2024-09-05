ca105e9080ae059ceaa4b8040f60e05bc7648381
stjernholm <stjernholm@msn.com>
2024-09-05 19:34:53 +0200

    feat: Add userParams hook

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export function UserProfile() {
+    const params = useParams();
     return (
         <>
-            <h2 className="UserProfile">This is the UserProfile</h2>
+            <h2 className="UserProfile">
+                This is the profile for user "{params.id}"
+            </h2>
         </>
     );
 }
